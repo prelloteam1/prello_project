c08211d8d1ae27719182f38a40bcbca7798ce047
alix.mfs@gmail.com <alix.mfs@gmail.com>
2023-03-07 15:43:26 +0000

    Change the "-" in CONCAT by "_"

--- dbt_prello/models/staging/stg_average_salary_by_municipality.sql
+++ dbt_prello/models/staging/stg_average_salary_by_municipality.sql
@@ -1,5 +1,5 @@
 SELECT 
-    CONCAT(municipality_code,"-", year) AS municipality_year_id,
+    CONCAT(municipality_code,"_", year) AS municipality_year_id,
     municipality_code,
     avg_net_salary,
     year AS year_year,
