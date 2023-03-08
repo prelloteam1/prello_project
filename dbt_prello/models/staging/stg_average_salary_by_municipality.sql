4973e1da8b1e6c0e84cc3b94a80122a19344167e
prello.team1@gmail.com <prello.team1@gmail.com>
2023-03-08 14:12:15 +0000

    Correcting date format & Adding package

--- dbt_prello/models/staging/stg_average_salary_by_municipality.sql
+++ dbt_prello/models/staging/stg_average_salary_by_municipality.sql
@@ -2,7 +2,7 @@ SELECT
     CONCAT(municipality_code,"_", year) AS municipality_year_id,
     municipality_code,
     avg_net_salary,
-    year AS year_year,
+    PARSE_DATE("%Y",CAST(year AS STRING)) AS year_year,
     country_code
 FROM
     prello_project.average_salary_by_municipality
--- dbt_prello/models/staging/stg_housing_stock.sql
+++ dbt_prello/models/staging/stg_housing_stock.sql
@@ -1,7 +1,7 @@
 SELECT
     int64_field_0 AS municipality_year_id,
     municipality_code,
-    year AS year_year,
+    PARSE_DATE("%Y",CAST(year AS STRING)) AS year_year,
     nb_principal_home,
     nb_second_home,
     nb_vacants_housing AS nb_vacants_home,
