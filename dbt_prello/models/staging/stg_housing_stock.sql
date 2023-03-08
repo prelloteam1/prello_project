709d74fdfa9c0fc93be9241fa03d574a89191093
prello.team1@gmail.com <prello.team1@gmail.com>
2023-03-08 09:25:08 +0000

    Correction on housing_stock / adding primary key & description of rate

--- dbt_prello/models/staging/stg_housing_stock.sql
+++ dbt_prello/models/staging/stg_housing_stock.sql
@@ -1,11 +1,16 @@
 SELECT
-    CONCAT(municipality_code,"_", year) AS municipality_year_id,
+    int64_field_0 AS municipality_year_id,
     municipality_code,
     year AS year_year,
     nb_principal_home,
     nb_second_home,
     nb_vacants_housing AS nb_vacants_home,
     nb_tot_housing AS nb_tot_home,
+    principal_home_rate,
+    secondary_home_rate,
+    vacants_housing_rate,
     country_code
+
 FROM
     prello_project.housing_stock
+		
