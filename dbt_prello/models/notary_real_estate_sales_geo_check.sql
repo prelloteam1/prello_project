6b91f6487f150848fb3f2877c85abedf9be55963
claireaux.vincent2@gmail.com <claireaux.vincent2@gmail.com>
2023-03-08 15:25:44 +0000

    notary geolocation checked and creation of schema.yml for models

---
+++ dbt_prello/models/notary_real_estate_sales_geo_check.sql
@@ -0,0 +1,21 @@
+
+SELECT 
+    n.sales_date,
+    n.sales_amount,
+    n.street_number,
+    n.street_code,
+    n.street_name,
+    n.nom_commune,
+    n.municipality_code,
+    n.premise_type,
+    n.surface,
+    n.number_of_principal_rooms,
+    n.sales_price_m2,
+    p.latitude,
+    p.longitude,
+    LEFT(municipality_code,2) AS department_code
+
+FROM    {{ ref ('stg_notary_real_estate_sales')}} n 
+LEFT JOIN {{ ref ('stg_geographical_referential')}} p 
+ON n.municipality_code = p.municipality_code
+
