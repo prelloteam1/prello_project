8553f8628e98017db6e4f2625d2fae8d31db0aea
alix.mfs@gmail.com <alix.mfs@gmail.com>
2023-03-09 16:52:09 +0000

    add ratio table for kpi of touristic sites

--- dbt_prello/models/staging/stg_geographical_referential.sql
+++ dbt_prello/models/staging/stg_geographical_referential.sql
@@ -6,15 +6,7 @@ SELECT
     latitude,
     longitude,
     department_code,
-    CASE
-        WHEN epci_code IS NULL AND department_code = '13' THEN '241300391'
-        WHEN epci_code IS NULL AND department_code = '75' THEN '200054781'
-        WHEN epci_code IS NULL AND department_code = '69' THEN '200046977'
-        WHEN epci_code IS NULL AND department_code = '22' THEN 'ZZZZZZZZZ'
-        WHEN epci_code IS NULL AND department_code = '85' THEN 'ZZZZZZZZZ'
-        WHEN epci_code IS NULL AND department_code = '29' THEN 'ZZZZZZZZZ'
-        ELSE CAST(epci_code AS STRING)
-    END AS epci_code,
+    epci_code,
     country_code,
     department_name
 FROM   
