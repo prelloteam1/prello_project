cf81bc708c29ab1400090c18b6b906af7362cd69
alix.mfs@gmail.com <alix.mfs@gmail.com>
2023-03-10 09:57:38 +0000

    Add the normalisation of the attractiveness for touristic sites

--- dbt_prello/models/ratio_touristic_sites.sql
+++ dbt_prello/models/ratio_touristic_sites.sql
@@ -12,13 +12,14 @@ WITH ratio_touristic_sites AS (
         municipality_code,
         COUNT(DISTINCT name) AS nb_touristic_sites,
         SUM(poi_notation) AS attractiveness, 
-        COUNT(DISTINCT name) * SUM(poi_notation) AS attractiveness_score
+        COUNT(DISTINCT name) * SUM(poi_notation) AS score_attractiveness
     FROM {{ref('POI_touristic_sites_by_municipality')}}
     GROUP BY department_code, department_name, epci_code, municipality_code
 )
 
 SELECT 
     *,
-    DENSE_RANK() OVER(ORDER BY attractiveness_score DESC) AS attractiveness_rank
+    DENSE_RANK() OVER(ORDER BY score_attractiveness DESC) AS attractiveness_rank,
+    SAFE_DIVIDE(score_attractiveness, 1547340) AS normalisation_attractiveness
 FROM ratio_touristic_sites
 ORDER BY attractiveness_rank
