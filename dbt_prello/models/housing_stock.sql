9f74442be8080bf600f35c8fc3a97acad867363c
claireaux.vincent2@gmail.com <claireaux.vincent2@gmail.com>
2023-03-10 13:13:04 +0000

    modifying housing stock

--- dbt_prello/models/housing_stock.sql
+++ dbt_prello/models/housing_stock.sql
@@ -4,5 +4,7 @@
 
 SELECT 
     *,
-    LEFT(municipality_code,2) AS department_code 
-FROM {{ ref ('stg_housing_stock')}}
+    LEFT(municipality_code,2) AS department_code,
+    p.department_name
+FROM {{ ref ('stg_housing_stock')}} a
+    LEFT JOIN {{ ref ('stg_geographical_referential')}} p USING(municipality_code)
