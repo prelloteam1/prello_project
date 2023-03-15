a6db3352671eb193853051e6b7f687c67b599f3f
prello.team1@gmail.com <prello.team1@gmail.com>
2023-03-15 09:37:52 +0000

    Merging_table update with touristic information

--- dbt_prello/models/merging_table.sql
+++ dbt_prello/models/merging_table.sql
@@ -8,8 +8,9 @@ SELECT
     p.normalized_profitability,
     r.nb_establishment,
     r.population,
-    t.attractiveness_rank,
+    t.normalized_ppe,
     t.normalisation_attractiveness,
+    t.global_score,
     tr.voyageurs AS has_train_station
     
 FROM 
@@ -17,6 +18,6 @@ FROM
     LEFT JOIN {{ ref ('occupation_rate_repartition_secondary_house') }} h USING(municipality_code)
     LEFT JOIN {{ ref ('profitability_m2') }} p USING(municipality_code)
     LEFT JOIN {{ ref ('ratio_establishment_by_population') }} r USING(municipality_code)
-    LEFT JOIN {{ ref ('ratio_touristic_sites') }} t USING(municipality_code)
+    LEFT JOIN {{ ref ('ratio_global_tourists') }} t USING(municipality_code)
     LEFT JOIN {{ ref ('stg_train_station') }} tr USING(city_name_normalized) 
 
